2425a7f9b37e217fd4e2dc086f02961e02eaf309
jac <joshuacarpentier21@gmail.com>
2020-04-07 23:17:16 -0400

    Add project files.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnUpdateDB = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(485, 567);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Fill";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column2,
+            this.Column1,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(69, 132);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(996, 342);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "CustomerAddress";
+            this.Column2.HeaderText = "Client Address";
+            this.Column2.Name = "Column2";
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "CustomerName";
+            this.Column1.HeaderText = "Client Name";
+            this.Column1.Name = "Column1";
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "CustomerState";
+            this.Column3.HeaderText = "State";
+            this.Column3.Name = "Column3";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(169, 71);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "State:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(263, 62);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(603, 62);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(132, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(507, 71);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Name:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(83, 567);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 28);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Show DT(1,2)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(275, 655);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(188, 28);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Change DT(1,2)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnUpdateDB
+            // 
+            this.btnUpdateDB.Location = new System.Drawing.Point(834, 567);
+            this.btnUpdateDB.Name = "btnUpdateDB";
+            this.btnUpdateDB.Size = new System.Drawing.Size(97, 42);
+            this.btnUpdateDB.TabIndex = 8;
+            this.btnUpdateDB.Text = "Update Database";
+            this.btnUpdateDB.UseVisualStyleBackColor = true;
+            this.btnUpdateDB.Click += new System.EventHandler(this.btnUpdateDB_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(623, 640);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 28);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Commit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1099, 793);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnUpdateDB);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnUpdateDB;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        DataTable mytable = new DataTable();
+        SqlConnection myconn = new SqlConnection();
+        SqlTransaction mytxn;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //  Establish a connection
+        
+            myconn.ConnectionString = " Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\pvfc\\PVFC.mdf;Integrated Security=True;Connect Timeout=30";
+            myconn.Open();
+           
+
+            //  Make a SQLCommand object
+
+            SqlCommand mycommand = new SqlCommand();
+          //  mycommand.CommandText = " Select * from Customer_T where CustomerState = '" + textBox1.Text + "'";
+            
+
+            mycommand.CommandText = " Select * from Customer_T with (READUncommitted)  ";
+
+
+            mycommand.Parameters.Add("@state", SqlDbType.NChar,20);
+            mycommand.Parameters["@state"].Value = textBox1.Text;
+            mycommand.Parameters.Add("@name", SqlDbType.NVarChar, 50);
+            mycommand.Parameters["@name"].Value = "%" + textBox2.Text + "%";  
+            mycommand.Connection = myconn;
+
+            //   Create an Adapter  (messenger carrying our request)
+
+            SqlDataAdapter myadapter = new SqlDataAdapter();
+            myadapter.SelectCommand = mycommand;
+
+            //   Fill an internal table
+           
+            myadapter.Fill(mytable);
+
+            //   Bind the table to the GUI object
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.DataSource = mytable;
+            
+
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(mytable.Rows[1].ItemArray[2].ToString());
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            mytable.Rows[1].SetField(2,"ABC");
+        }
+
+        private void btnUpdateDB_Click(object sender, EventArgs e)
+        {
+
+            //    updcmd.CommandText = "Update Customer_T set CustomerName = @customername," + "CustomerAddress = @customeraddress, CustomerCity = @customercity,"
+            //+ " CustomerState = @customerstate, CustomerPostalCode = @customerpostalcode" + " where CustomerID = @customerid";
+
+
+            mytxn = myconn.BeginTransaction(IsolationLevel.ReadUncommitted);
+            SqlCommand updcmd;   
+            updcmd = new SqlCommand();
+            updcmd.Transaction = mytxn;
+            updcmd.Connection = myconn;
+            updcmd.CommandText = "Update Customer_T set CustomerName = @customername " + "where CustomerID = @customerid and" +
+                " CustomerVersion = @version";
+            updcmd.Parameters.Add("@version", SqlDbType.Binary, 50, "CustomerVersion");
+            updcmd.Parameters.Add("@customername", SqlDbType.NVarChar, 50,"CustomerName");
+            updcmd.Parameters.Add("@customerid", SqlDbType.Int, 50, "CustomerID");
+            SqlDataAdapter myadapter = new SqlDataAdapter();
+            myadapter.UpdateCommand = updcmd;
+          
+            SqlCommand delcmd;
+            delcmd = new SqlCommand();
+            delcmd.Connection = myconn;
+            delcmd.CommandText = "Delete Customer_T where CustomerID = @customerid ";
+            delcmd.Parameters.Add("@customerid", SqlDbType.Int, 50, "CustomerID");
+            
+            myadapter.DeleteCommand = delcmd;
+            myadapter.ContinueUpdateOnError = true;
+
+            try
+            {
+                myadapter.Update(mytable);
+                
+
+                //mytxn.Commit();
+            }
+            catch (Exception ex)
+            {
+                mytxn.Rollback();
+                MessageBox.Show("Database has been updated - Please refill grid and make updates");
+                // do the Fill (from above) again
+            }
+
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            mytxn.Commit();
+        }
+    }
+}
